3fae3a2e197a17e7fe124f084249c97b960d4ef7
rajeshri.nevare <rajeshri.nevare@ibm.com>
2023-01-10 17:09:26 +0530

    Project setup

---
+++ index.ts
@@ -0,0 +1,3 @@
+const emp = { name : 'Rajeshri', age :35 };
+emp.age = 40;
+console.log(emp.age);
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata'; // We need this in order to use @Decorators
+import express, { Application } from 'express';
+import { config } from '../src/config';
+import { loggerDev } from '../src/utils/logger';
+import { loaders } from '../src/loaders';
+
+const startServer = async () => {
+  const app: Application = express();
+  await loaders(app);
+  loggerDev.debug(`MODE ENV ${process.env.NODE_ENV}`);
+  app
+    .listen(config.port, () => {
+      loggerDev.info(`Server listening on port: ${config.port}`);
+    })
+    .on('error', err => {
+      loggerDev.error(err);
+      process.exit(1);
+    });
+};
+
+startServer();
